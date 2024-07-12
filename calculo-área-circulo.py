d0545ed082e0b7fedcc90f57cb76dd9b30a7634c
Élaff Ramos <valelaff10@gmail.com>
2024-07-12 08:31:43 -0300

    Create calculo-área-circulo.py

---
+++ calculo-área-circulo.py
@@ -0,0 +1,8 @@
+# Calculo área de um circulo!
+
+print('***CALCULANDO A ÁREA DO CIRCULO***')
+
+raio = float(input('Digite o valor do raio: '))
+pi =3.1 
+
+print('Área do circulo '+str(pi*raio**2)) #Calculo ao quadrado
